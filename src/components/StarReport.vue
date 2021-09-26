897649c0b1c95d36cba8ec648f6cfff3e9511bc1
bell-rabbit <andy19974a22@gmail.com>
2021-09-26 23:38:50 +0800

    fix(StarReport) add star5 up

---
+++ src/components/StarReport.vue
@@ -0,0 +1,54 @@
+<template>
+  <v-data-table
+      :headers="headers"
+      :items="desserts"
+      :items-per-page="-1"
+      mobile-breakpoint="0"
+      hide-default-footer
+  >
+    <template v-slot:foot v-if="rollTotal > 0">
+      <tr>
+        <td></td>
+        <td></td>
+        <td colspan="2" class="text-right">
+          你已抽取{{ rollTotal }}次
+        </td>
+      </tr>
+    </template>
+  </v-data-table>
+</template>
+
+<script>
+export default {
+  name: 'StarReport',
+  props: {
+    rollTotal: {
+      type: Number,
+      default: 0
+    },
+    desserts:{
+      type: Array
+    }
+  },
+  data () {
+    return {
+      headers: [
+        {
+          text: '',
+          align: 'start',
+          sortable: false,
+          value: 'title',
+        },
+        { text: '5星 Up', value: 'star5_up', sortable: false, align: 'end' },
+        { text: '5星', value: 'star5', sortable: false, align: 'end' },
+        { text: '4星', value: 'star4', sortable: false, align: 'end' },
+        { text: '3星', value: 'star3', sortable: false, align: 'end' }
+      ],
+    };
+  },
+};
+</script>
+
+<style scoped>
+
+</style>
--- src/views/Flipper.vue
+++ src/views/Flipper.vue
@@ -10,25 +10,7 @@
             <v-card-text class="mb-0 pb-0">此結果為這個網站第 <span style="color: #ffcd76">{{ total }}</span> 次模擬
             </v-card-text>
             <v-card-text>
-
-              <v-data-table
-                  :headers="headers"
-                  :items="desserts"
-                  :items-per-page="-1"
-                  mobile-breakpoint="0"
-                  hide-default-footer
-              >
-                <template v-slot:foot>
-                  <tr>
-                    <td></td>
-                    <td></td>
-                    <td colspan="2" class="text-right">
-                      你已抽取{{ rollTotal }}次
-                    </td>
-                  </tr>
-                </template>
-              </v-data-table>
-
+              <star-report :desserts="desserts" :roll-total="rollTotal"/>
             </v-card-text>
           </v-card>
         </v-row>
@@ -51,10 +33,11 @@
 import API from '../plugins/api';
 import RollButton from '@/components/RollButton';
 import ResultCard from '@/components/ResultCard';
+import StarReport from '@/components/StarReport';
 
 export default {
   name: 'Flipper',
-  components: { ResultCard, RollButton },
+  components: { StarReport, ResultCard, RollButton },
   props: {
     pool: {
       type: String,
@@ -67,17 +50,6 @@ export default {
       isRoll: false,
       result: [],
       total: 0,
-      headers: [
-        {
-          text: '',
-          align: 'start',
-          sortable: false,
-          value: 'title',
-        },
-        { text: '5星', value: 'star5', sortable: false, align: 'end' },
-        { text: '4星', value: 'star4', sortable: false, align: 'end' },
-        { text: '3星', value: 'star3', sortable: false, align: 'end' }
-      ],
       desserts: [],
       rollTotal: 0
     };
@@ -85,23 +57,25 @@ export default {
   methods: {
     roll () {
       API.roll(this.pool).then((rs) => {
-        let report = { '3': 0, '4': 0, '5': 0 };
-        let count = this.desserts[1] ? this.desserts[1] : { title: '總數目', star5: 0, star4: 0, star3: 0 };
+        let report = { '3': 0, '4': 0, '5': 0, '5-pu': 0 };
+        let count = this.desserts[1] ? this.desserts[1] : { title: '總數目', star5_up: 0, star5: 0, star4: 0, star3: 0 };
 
         rs.data.forEach(i => {
           report[i.rarity]++;
         });
 
+        count.star5_up += report['5-pu'];
         count.star5 += report['5'];
         count.star4 += report['4'];
         count.star3 += report['3'];
 
-        let countTotal = count.star5 + count.star4 + count.star3;
+        let countTotal = count.star5_up + count.star5 + count.star4 + count.star3;
         this.desserts = [
-          { title: '本次', star5: report['5'], star4: report['4'], star3: report['3'] },
+          { title: '本次', star5_up: report['5-pu'], star5: report['5'], star4: report['4'], star3: report['3'] },
           count,
           {
             title: '機率',
+            star5_up: `${(Math.round((count.star5_up / countTotal) * 10000) / 100).toFixed(2)}%`,
             star5: `${(Math.round((count.star5 / countTotal) * 10000) / 100).toFixed(2)}%`,
             star4: `${(Math.round((count.star4 / countTotal) * 10000) / 100).toFixed(2)}%`,
             star3: `${(Math.round((count.star3 / countTotal) * 10000) / 100).toFixed(2)}%`
