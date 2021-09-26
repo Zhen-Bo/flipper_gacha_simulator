a42c72f6b48705281d80ab3b0f1fafa66f8c1b53
bell-rabbit <andy19974a22@gmail.com>
2021-09-27 00:52:35 +0800

    Package(Moment) add moment

--- src/main.js
+++ src/main.js
@@ -2,8 +2,11 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import vuetify from './plugins/vuetify'
+import moment from 'vue-moment'
 
 Vue.config.productionTip = false
+
+Vue.use(moment);
 
 new Vue({
   router,
