6657b13d55e3e9b55b93ae5e4ce9dc18d9e92a25
bell-rabbit <andy19974a22@gmail.com>
2021-09-27 00:51:48 +0800

    feat(History) add view History

--- src/views/History.vue
+++ src/views/History.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="grey lighten-4">
-    <AppNav/>
+
     <v-container class="grey lighten-4">
       <v-row>
         <v-text-field
@@ -14,19 +14,28 @@
       </v-row>
     </v-container>
 
-
-    <v-card class=" mt-2" >
-      <v-card-title>你的最近10個記錄</v-card-title>
-      <v-card-text>
-        <v-sheet
-            v-for="n in 10"
-            :key="n"
+    <v-card class=" mt-2">
+      <v-card-title>你的最近{{ total }}個記錄
+        <v-spacer/>
+        <tooltip-icon-button icon="mdi-delete" message="刪除記錄" @click="deleteHistory"/>
+      </v-card-title>
+      <v-card-text class="pl-0 pr-0">
+        <star-report :desserts="reportDesserts"/>
+        <v-divider/>
+        <v-timeline
+            align-top
+            dense
+            v-if="total > 0"
         >
-          <v-skeleton-loader
-              class="mx-auto"
-              type="card"
-          ></v-skeleton-loader>
-        </v-sheet>
+          <v-timeline-item
+              v-for="item in items" :key="item.dateTime"
+              :color="dotColor[getTopStar(item.data)]"
+              :small="getTopStar(item.data) !== '5-pu'"
+          >
+            <strong>{{ item.dateTime  | moment('MMMM Do YYYY, h:mm:ss a') }}</strong>
+            <result-card style="zoom:0.8;" :character-list="item.data"></result-card>
+          </v-timeline-item>
+        </v-timeline>
 
       </v-card-text>
     </v-card>
@@ -35,17 +44,112 @@
 </template>
 
 <script>
-import AppNav from '@/components/Nav';
+import ResultCard from '@/components/ResultCard';
+import StarReport from '@/components/StarReport';
+import TooltipIconButton from '@/components/TooltipIconButton';
 
 export default {
   name: 'History',
-  components: { AppNav },
+  components: { TooltipIconButton, StarReport, ResultCard },
   data () {
     return {
-      items: [
-        { name: 'machine_police_girl', text: '警察池' }, { name: 'halfanv', text: '半周年禮黑' }
-      ]
+      total: 0,
+      /** @type {array<{data:array<character>,  dateTime:string}>} */
+      items: [],
+      reportDesserts: [],
+      dotColor: { '5-pu': 'red darken-3', '5': 'red lighten-2', '4': 'orange lighten-2', '3': 'white' }
     };
   },
+  props: {
+    pool: {
+      type: String,
+      default: ''
+    },
+  },
+  methods: {
+    deleteHistory () {
+      localStorage.removeItem(this.pool);
+      this.loadLocalStorage();
+    },
+    /** @param {array<character>} characterList */
+    getTopStar (characterList) {
+      let top = 0;
+      for (let i = 0; i < characterList.length; i++) {
+        let rs = characterList[i];
+        switch (rs.rarity) {
+          case '5-pu':
+            return '5-pu';
+          case '5':
+            if (top < 5) { top = 5;}
+            break;
+          case '4':
+            if (top < 4) { top = 4;}
+            break;
+          case '3':
+            if (top < 3) { top = 3;}
+            break;
+        }
+      }
+      return '' + top;
+    },
+    updateReport () {
+      let count = { title: '總數目', star5_up: 0, star5: 0, star4: 0, star3: 0 };
+      let countTotal = 0;
+
+      this.items.forEach((rs) => {
+        countTotal++;
+
+        rs.data.forEach((ch) => {
+          switch (ch.rarity) {
+            case '5-pu':
+              count.star5_up++;
+              break;
+            case '5':
+              count.star5++;
+              break;
+            case '4':
+              count.star4++;
+              break;
+            case '3':
+              count.star3++;
+              break;
+          }
+        });
+      });
+
+      this.reportDesserts = [
+        count,
+        {
+          title: '機率',
+          star5_up: `${(Math.round((count.star5_up / (countTotal * 10)) * 10000) / 100).toFixed(2)}%`,
+          star5: `${(Math.round((count.star5 / (countTotal * 10)) * 10000) / 100).toFixed(2)}%`,
+          star4: `${(Math.round((count.star4 / (countTotal * 10)) * 10000) / 100).toFixed(2)}%`,
+          star3: `${(Math.round((count.star3 / (countTotal * 10)) * 10000) / 100).toFixed(2)}%`
+        }
+      ];
+    },
+    clear () {
+      this.total = 0;
+      this.items = [];
+      this.reportDesserts = [
+        { title: '總數目', star5_up: 0, star5: 0, star4: 0, star3: 0 },
+        { title: '機率', star5_up: '0.00%', star5: '0.00%', star4: '0.00%', star3: '0.00%' }
+      ];
+    },
+    loadLocalStorage () {
+      let item = localStorage.getItem(this.pool);
+      if (!item) {
+        this.clear();
+        return;
+      }
+
+      this.items = JSON.parse(item);
+      this.total = this.items.length;
+      this.updateReport();
+    }
+  },
+  created () {
+    this.loadLocalStorage();
+  }
 };
 </script>
