7482b54862d22d5af7250473b200fdb217914f87
bell-rabbit <andy19974a22@gmail.com>
2021-09-26 17:10:10 +0800

    feat(resource) add resourceURL in .env

--- src/plugins/api.js
+++ src/plugins/api.js
@@ -1,6 +1,8 @@
 let baseURL = process.env.VUE_APP_HOST_NAME || '';
+let resourceURL = process.env.VUE_APP_RESOURCE_URL || '';
 
 export default {
+  resourceURL,
   /**
    * @param {string} pool
    * @returns {Promise<{data:Array<{attri: string, id: string, name: string, rarity:string}>, total:number, report: {all_five: number, all_four: number, all_roll: number, all_three: number}}>}
@@ -11,6 +13,10 @@ export default {
   },
   report (pool) {
     return fetch(`${baseURL}/wf/result?pool=${pool}&data_mode=true`)
+      .then(res => res.json());
+  },
+  pool(){
+    return fetch(`${baseURL}/wf/result?get_pool=true`)
       .then(res => res.json());
   }
 };
