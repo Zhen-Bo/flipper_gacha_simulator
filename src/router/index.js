87b2656b52c4cebf5dd0371303f75e288e28f333
bell-rabbit <andy19974a22@gmail.com>
2021-09-26 14:52:30 +0800

    feat(router) update the router

--- src/router/index.js
+++ src/router/index.js
@@ -1,22 +1,28 @@
-import Vue from 'vue'
-import VueRouter from 'vue-router'
+import Vue from 'vue';
+import VueRouter from 'vue-router';
 
-import flipper from '../views/Flipper'
-import history from '../views/History'
-import report from '../views/Report'
+import flipper from '@/views/Flipper';
+import history from '@/views/History';
+import report from '@/views/Report';
 
-Vue.use(VueRouter)
+import nav from '@/views/Nav';
+
+Vue.use(VueRouter);
 
 const routes = [
-  { path: '/',  redirect: '/flipper' },
-  {path: '/flipper', name: 'Flipper', component: flipper},
-  {path: '/history', name: 'History', component: history},
-  {path: '/report', name: 'Report', component: report},
-]
+  {
+    path: '/', name: 'Nav', component: nav, children: [
+      { path: '/flipper', name: 'Flipper', component: flipper },
+      { path: '/history', name: 'History', component: history },
+      { path: '/report', name: 'Report', component: report },
+      { path: '*', redirect: { name: 'Flipper' } }
+    ]
+  }
+];
 
 const router = new VueRouter({
   mode: 'history',
   routes
-})
+});
 
-export default router
+export default router;
