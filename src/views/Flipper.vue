c7145c6eb8edfb8f481d4ad292c7dd27ce42759b
bell-rabbit <andy19974a22@gmail.com>
2021-09-26 17:09:21 +0800

    feat(flipper) remove flipper nav

--- src/views/Flipper.vue
+++ src/views/Flipper.vue
@@ -1,42 +1,6 @@
 <template>
   <div>
-    <v-toolbar
-        color="primary"
-        dark
-        flat
-    >
-      <v-toolbar-title>彈射抽卡模擬</v-toolbar-title>
-
-      <v-spacer></v-spacer>
-      <v-btn icon href="https://github.com/Zhen-Bo/flipper_gacha_simulator/issues" target="_blank">
-        <v-icon>mdi-bug</v-icon>
-      </v-btn>
-
-      <template v-slot:extension>
-        <v-tabs
-            v-model="tab"
-            align-with-title
-        >
-          <v-tabs-slider color="yellow"></v-tabs-slider>
-
-          <v-tab
-              v-for="item in items"
-              :key="item.name"
-          >
-            {{ item.text }}
-          </v-tab>
-        </v-tabs>
-      </template>
-    </v-toolbar>
-    <v-tabs-items v-model="tab">
-      <v-tab-item
-          v-for="item in items"
-          :key="item.name"
-      >
-        <v-img
-            :src='`https://raw.githubusercontent.com/Zhen-Bo/flipper_gacha_simulator/main/static/image/pool_image/${item.name}.png`'
-        >
-        </v-img>
+        <v-img :src='`${resourceURL}/static/image/pool_image/${pool}.png`'></v-img>
 
         <v-img
             class="mt-2"
@@ -46,7 +10,7 @@
             width="440px"
         >
           <v-container class="mt-2">
-            <v-row class="justify-center mb-3" v-for="(row, index) in [3, 4, 3]" :key="`row_${item.name}_${row}`">
+            <v-row class="justify-center mb-3" v-for="(row, index) in [3, 4, 3]" :key="`row_${pool}_${row}`">
               <character-icon v-for="n in row" :key="n"
                               :attri="result[index][n]? result[index][n].attri: ''"
                               :name="result[index][n]? result[index][n].name: ''"
@@ -59,7 +23,7 @@
         <v-img :src='require("..\\assets\\bg.jpg")'>
           <v-container class="justify-center mt-3">
             <v-row class="justify-center mb-3">
-              <v-card width="400px" v-if="isRoll[tab]">
+              <v-card width="400px" v-if="isRoll">
                 <v-card-text class="mb-0 pb-0">此結果為這個網站第 <span style="color: #ffcd76">{{ total }}</span> 次模擬
                 </v-card-text>
                 <v-card-text>
@@ -84,14 +48,10 @@
 
                 </v-card-text>
               </v-card>
-
             </v-row>
 
-
             <v-row class="justify-space-around mb-2">
-              <v-btn text>
-
-              </v-btn>
+              <v-btn text/>
               <roll-button @click="roll"/>
               <v-btn height="64px">
                 <v-icon>
@@ -101,28 +61,27 @@
             </v-row>
           </v-container>
         </v-img>
-
-      </v-tab-item>
-    </v-tabs-items>
   </div>
 </template>
 
 <script>
-import Api from '../plugins/api';
+import API from '../plugins/api';
 import RollButton from '@/components/RollButton';
 import CharacterIcon from '@/components/CharacterIcon';
 
 export default {
   name: 'Flipper',
   components: { CharacterIcon, RollButton },
+  props: {
+    pool: {
+      type: String,
+      default: ''
+    },
+  },
   data () {
     return {
-      tab: null,
-      items: [
-        { name: 'machine_police_girl', text: '警察池' }, { name: 'halfanv', text: '半周年禮黑' }
-      ],
-      isRoll: [false, false],
-      text: 'Lorem ipsum dolor ',
+      resourceURL: API.resourceURL,
+      isRoll: false,
       result: [[], [], []],
       resultReport: [0, 0, 0],
       total: 0
@@ -130,7 +89,7 @@ export default {
   },
   methods: {
     roll () {
-      Api.roll(this.items[this.tab].name).then((rs) => {
+      API.roll(this.pool).then((rs) => {
 
         let rollDate = [[], [], []];
         let report = { '3': 0, '4': 0, '5': 0 };
@@ -157,9 +116,9 @@ export default {
         this.total = rs.total;
         this.resultReport = [report['5'], report['4'], report['3']];
         this.result = rollDate;
-        this.isRoll[this.tab] = true;
+        this.isRoll = true;
       });
     }
-  },
+  }
 };
 </script>
