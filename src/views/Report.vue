63326e3fab3c66dfc3c17261fb0f43fb17770e74
bell-rabbit <andy19974a22@gmail.com>
2021-09-28 00:37:05 +0800

    feat(Report) update the get all start report

--- src/views/Report.vue
+++ src/views/Report.vue
@@ -1,20 +1,12 @@
 <template>
   <div>
-    <AppNav>
-    </AppNav>
-    <v-card class="mt-2" >
+    <v-card class="mt-2">
       <v-card-text class="pl-0 pr-0">
-        <v-data-table
-            :headers="headers"
-            :items="desserts"
-            :items-per-page="-1"
-            mobile-breakpoint="0"
-            hide-default-footer
-        ></v-data-table>
+        <star-report :desserts="rollDesserts" :five-pu="false"></star-report>
       </v-card-text>
     </v-card>
 
-    <v-card class="mt-2" >
+    <v-card class="mt-2">
       <v-card-title class="pb-0">最常出現的5星</v-card-title>
       <v-card-text class="pl-0 pr-0">
         <v-data-table
@@ -26,10 +18,10 @@
         >
           <template v-slot:item.character="{ item }">
             <character-icon
-                            :attri="item.character.attri"
-                            :name="item.character.name"
-                            :id="item.character.id"
-                            :rarity="item.character.rarity"/>
+                :attri="item.character.attri"
+                :name="item.character.name"
+                :id="item.character.id"
+                :rarity="item.character.rarity"/>
           </template>
 
         </v-data-table>
@@ -40,30 +32,22 @@
 </template>
 
 <script>
-import AppNav from '@/components/Nav';
+import API from '@/plugins/api';
 import CharacterIcon from '@/components/CharacterIcon';
+import StarReport from '@/components/StarReport';
 
 export default {
   name: 'Report',
-  components: { CharacterIcon, AppNav },
+  components: { StarReport, CharacterIcon },
+  props: {
+    pool: {
+      type: String,
+      default: ''
+    },
+  },
   data () {
     return {
-      headers: [
-        {
-          text: '稀有度',
-          align: 'start',
-          sortable: false,
-          value: 'star',
-        },
-        { text: '總出貨次數', value: 'all' },
-        { text: '總出貨率', value: 'rate' }
-      ],
-      desserts: [
-        { star: '5星', all: '63767', rate: '4.997%' },
-        { star: '4星', all: '408508', rate: '32.013%' },
-        { star: '3星', all: '803795', rate: '62.990%' },
-        { star: '統計', all: '1276070', rate: '100.000%' }
-      ],
+      rollDesserts:[],
       headers1: [
         {
           text: '角色',
@@ -75,11 +59,26 @@ export default {
         { text: '總出貨率', value: 'rate' }
       ],
       desserts1: [
-        { character: {"name":"水前","id":"onmyoji_boy","attri":"Water","rarity":"5"}, name: '水前', rate: '40%' },
-        { character: {"name":"列恩","id":"mercenary","attri":"Wind","rarity":"5"}, name: '風叔', rate: '30%' },
-        { character: {"name":"瓦格納","id":"fire_dragon","attri":"Fire","rarity":"5"}, name: '火龍', rate: '20%' },
+        { character: { 'name': '水前', 'id': 'onmyoji_boy', 'attri': 'Water', 'rarity': '5' }, name: '水前', rate: '40%' },
+        { character: { 'name': '列恩', 'id': 'mercenary', 'attri': 'Wind', 'rarity': '5' }, name: '風叔', rate: '30%' },
+        { character: { 'name': '瓦格納', 'id': 'fire_dragon', 'attri': 'Fire', 'rarity': '5' }, name: '火龍', rate: '20%' },
       ],
     };
   },
+  created () {
+    API.report(this.pool).then((rs) => {
+      this.rollDesserts = [
+        { title: '總出貸次數', star5_up: '0', star5: rs.all_five, star4: rs.all_four, star3: rs.all_three },
+        {
+          title: '總出貸機率',
+          star5_up: '0',
+          star5: `${API.round(rs.all_five / rs.all_roll)}%`,
+          star4: `${API.round(rs.all_four / rs.all_roll)}%`,
+          star3: `${API.round(rs.all_three / rs.all_roll)}%`
+        },
+      ];
+      console.log(this.rollDesserts);
+    });
+  }
 };
 </script>
