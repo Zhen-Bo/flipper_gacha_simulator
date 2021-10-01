e27b1beaebf007badb931ddfbbec90d5b9650472
bell-rabbit <andy19974a22@gmail.com>
2021-10-01 22:26:07 +0800

    feat(Roll Button) update the resource png path

--- src/components/RollButton.vue
+++ src/components/RollButton.vue
@@ -1,16 +1,23 @@
 <template>
   <v-btn depressed height="66px" width="178px" @click="clickButton">
-    <img :src='require("..\\assets\\roll.png")' alt="roll"/>
+    <img :src='`${resourceURL}/static/roll.png`' alt="roll"/>
   </v-btn>
 </template>
 
 <script>
+import API from '../plugins/api';
+
 export default {
   name: 'RollButton',
   methods: {
     clickButton () {
       this.$emit("click");
     }
+  },
+  data () {
+    return {
+      resourceURL: API.resourceURL
+    };
   },
 }
 </script>
