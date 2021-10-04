abc496ff859fb0c0f4ddd6bc4925084063af3ee8
Zhen-Bo <sharekevin001958@gmail.com>
2021-10-04 14:26:05 +0800

    檔案微調

--- src/router/index.js
+++ src/router/index.js
@@ -1,28 +1,31 @@
-import Vue from 'vue';
-import VueRouter from 'vue-router';
+import Vue from "vue";
+import VueRouter from "vue-router";
 
-import flipper from '@/views/Flipper';
-import history from '@/views/History';
-import report from '@/views/Report';
+import flipper from "@/views/Flipper";
+import history from "@/views/History";
+import report from "@/views/Report";
 
-import nav from '@/views/Nav';
+import nav from "@/views/Nav";
 
 Vue.use(VueRouter);
 
 const routes = [
   {
-    path: '/', name: 'Nav', component: nav, children: [
-      { path: '/flipper', name: 'Flipper', component: flipper },
-      { path: '/history', name: 'History', component: history },
-      { path: '/report', name: 'Report', component: report },
-      { path: '*', redirect: { name: 'Flipper' } }
-    ]
-  }
+    path: "/wf",
+    name: "Nav",
+    component: nav,
+    children: [
+      { path: "/wf/flipper", name: "Flipper", component: flipper },
+      { path: "/wf/history", name: "History", component: history },
+      { path: "/wf/report", name: "Report", component: report },
+      { path: "*", redirect: { name: "Flipper" } },
+    ],
+  },
 ];
 
 const router = new VueRouter({
-  mode: 'history',
-  routes
+  mode: "history",
+  routes,
 });
 
 export default router;
