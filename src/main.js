a482df48189c30fdff7a4c626639d960761c1503
Zhen-Bo <sharekevin001958@gmail.com>
2021-10-29 09:34:46 +0800

    add vue-analytics

--- src/main.js
+++ src/main.js
@@ -1,15 +1,24 @@
-import Vue from 'vue'
-import App from './App.vue'
-import router from './router'
-import vuetify from './plugins/vuetify'
-import moment from 'vue-moment'
+import Vue from "vue";
+import VueAnalytics from "vue-analytics";
+import App from "./App.vue";
+import router from "./router";
+import vuetify from "./plugins/vuetify";
+import moment from "vue-moment";
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
+
+Vue.use(VueAnalytics, {
+  id: "UA-206510416-4",
+  router,
+  autoTracking: {
+    pageviewOnLoad: false,
+  },
+});
 
 Vue.use(moment);
 
 new Vue({
   router,
   vuetify,
-  render: h => h(App)
-}).$mount('#app')
+  render: (h) => h(App),
+}).$mount("#app");
